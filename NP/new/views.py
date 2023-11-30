e455a590e583f8b12d5016b1c0bf1c9b6ac0a979
Name <email@example.ru>
2023-11-30 13:07:41 +0700

    newsPEP

---
+++ NP/new/views.py
@@ -0,0 +1,94 @@
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import TYPE
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+
+from .filters import PostFilter
+from .forms import PostForm
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-time_creation'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class NewsCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('new.add_post',
+                           'new.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.type = 'NW'
+        return super().form_valid(form)
+
+
+class ArticleCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('new.add_post',
+                           'new.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'article_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.type = 'AL'
+        return super().form_valid(form)
+
+
+class NewsUpdate(PermissionRequiredMixin, LoginRequiredMixin, UpdateView):
+    permission_required = ('new.add_post',
+                           'new.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+    context_object_name = 'news_edit'
+    success_url = reverse_lazy('post_list')
+
+
+class ArticleUpdate(PermissionRequiredMixin, LoginRequiredMixin, UpdateView):
+    permission_required = ('new.add_post',
+                           'new.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'article_edit.html'
+    context_object_name = 'article_edit'
+    success_url = reverse_lazy('post_list')
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('post_list')
+
+
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('post_list')
+
